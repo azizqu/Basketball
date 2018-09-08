6e2c349f279d44ea72636033dcc706b1d414ba5e
azizqu <azizqu@gmail.com>
2018-09-08 11:10:27 -0400

    Initial commit

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Ball ball;
+    public GameObject playerCamera;
+
+    public float ballDistance = 2f;
+
+    public float ballThrowingForce = 5f;
+
+    public bool holdingBall = true;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        ball.GetComponent<Rigidbody>().useGravity = false;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(holdingBall)
+        {
+            ball.transform.position = playerCamera.transform.position + playerCamera.transform.forward * ballDistance;  // position references point in the world; forward references the direction i.e up down left right; 
+
+            if(Input.GetMouseButton(0))
+            {
+                holdingBall = false; // release ball
+                ball.ActivateTrail();
+                ball.GetComponent<Rigidbody>().useGravity = true; // ball drops;
+
+                ball.GetComponent<Rigidbody>().AddForce(playerCamera.transform.forward * ballThrowingForce); // throw
+            }
+        }
+    }
+
+        
+}
